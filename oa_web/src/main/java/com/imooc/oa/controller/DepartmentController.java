a03b1d73c5813f735ba0b5a1b091d807dec52bee
fangfangxu <472848022@qq.com>
2020-02-13 00:55:43 +0800

    commit
    commit

--- oa_web/src/main/java/com/imooc/oa/controller/DepartmentController.java
+++ oa_web/src/main/java/com/imooc/oa/controller/DepartmentController.java
@@ -4,6 +4,7 @@ import com.imooc.oa.biz.DepartmentBiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 import java.util.Map;
@@ -19,5 +20,13 @@ private DepartmentBiz departmentBiz;
     public String list(Map<String,Object> map){
         map.put("list",departmentBiz.getAll());
         return "department_list";
+    }
+
+
+    @RequestMapping("/list1")
+    @ResponseBody
+    public String list1(Map<String,Object> map){
+        map.put("list",departmentBiz.getAll());
+        return "徐";
     }
 }
